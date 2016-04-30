da39c77e75caa625956a40b14570488f932a7584
Jeroen Druwé <jdruwe@Jeroens-MacBook-Pro-3.local>
2016-04-30 18:47:02 +0200

    Changed to scss and wrote my own simple test

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,22 +5,22 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello World!'
+  export default {
+    data () {
+      return {
+        // note: changing this line won't causes changes
+        // with hot-reload because the reloaded component
+        // preserves its current state and we are modifying
+        // its initial state.
+        msg: 'Hello World!'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  h1 {
+    color: red;
+  }
 </style>
--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Hello from 'src/components/Hello'
 
 describe('Hello.vue', () => {
+  it('should have correct message', () => {
+    expect(Hello.data().msg).to.be.equal('Hello World!')
+  })
   it('should render correct contents', () => {
     const vm = new Vue({
       template: '<div><hello></hello></div>',
