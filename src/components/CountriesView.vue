4cba862b65418ea8ba52baa37d828e9ba72cc6a5
Jeroen Druwé <jdruwe@Jeroens-MacBook-Pro-3.local>
2016-05-15 22:41:21 +0200

    Added country cmp

--- src/components/CountriesView.vue
+++ src/components/CountriesView.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
     <h1>Countries</h1>
+    <ul>
+      <country
+        v-for="country in countries"
+        :country="country">
+      </country>
+    </ul>
   </div>
 </template>
 
 <script>
+  import Country from './Country.vue'
+
   export default {
+    data () {
+      return {
+        countries: [
+          {name: 'Belgium'},
+          {name: 'Netherlands'},
+          {name: 'France'},
+          {name: 'Germany'}
+        ]
+      }
+    },
+    components: {
+      Country
+    }
   }
 </script>
 
---
+++ src/components/Country.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>County: {{country.name}}</li>
+</template>
+
+<script>
+  export default {
+    name: 'Country',
+    props: ['country']
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
