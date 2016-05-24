6afc2e4545fdd233a1035d6d76ba0a130c071b57
Jeroen Druwé <jdruwe@Jeroens-MacBook-Pro-3.local>
2016-05-24 14:03:23 +0200

    Updated about page

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -1,25 +1,43 @@
 <template>
-  <div>
+  <div class="about">
     <div class="header">
       <h3 class="header__title">About</h3>
     </div>
-    <div class="row">
-      <div class="col s10 offset-s1 m6 offset-m3 center">
-        This website was build by <a href="http://jeroendruwe.be" target="_blank">Jeroen Druwé</a> to demonstrate some of the potential of VueJS.
-        If you have any questions leave them in the comment section of the corresponding post on my website. The code van be found on
-        <a href="https://github.com/Jdruwe/travel-vue" target="_blank">Github</a>
-      </div>
-    </div>
+    <img class="about__image circle" src="/static/jeroen.jpg" alt="Jeroen Druwé">
+    <p>This website was build by <a href="http://jeroendruwe.be" target="_blank">Jeroen Druwé</a> to demonstrate some of
+      the potential of VueJS.
+      If you have any questions leave them in the comment section of the corresponding post on my website. The code van
+      be found on
+      <a href="https://github.com/Jdruwe/travel-vue" target="_blank">Github</a></p>
   </div>
 </template>
 
 <script>
-  export default {
-
-  }
+  export default {}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+
+  .about {
+
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 700px;
+
+  p {
+    text-align: center;
+  }
+
+  .about__image {
+    width: 150px;
+    height: 150px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  }
 
 </style>
